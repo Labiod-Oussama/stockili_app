a94a4fbbbcb7a5c8f47a95f4055195a1d2f9eacc
Oussama_LD <oussama.labiod@inttic.dz>
2023-08-21 20:30:38 +0100

    apt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/stockili_app/",
   plugins: [react()],
-  base: "/stockili_app"
 })
