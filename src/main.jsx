052bf5b3a3fb1d4953c4a9debf1e689745d1487a
Oussama_LD <oussama.labiod@inttic.dz>
2023-08-21 20:33:38 +0100

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/stockili_app",
-    element: <App />
-  }
-])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <RouterProvider router={router} />
+    <App />
   </>,
 )
